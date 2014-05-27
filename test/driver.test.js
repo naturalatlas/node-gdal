910489f69e177317b1d520be07c9831bccb3839a
Brian Reavis <brian@thirdroute.com>
2014-05-26 21:49:55 -0600

    Added driver tests for jpg/png.

--- test/driver.test.js
+++ test/driver.test.js
@@ -2,6 +2,7 @@
 
 var assert = require('assert');
 var gdal = require('../lib/gdal.js');
+var expect = require('chai').assert;
 
 describe('Driver', function() {
 	var expected = {
@@ -22,6 +23,18 @@ describe('Driver', function() {
 			DMD_MIMETYPE: undefined,
 			DMD_EXTENSION: undefined,
 			DCAP_CREATE: 'YES'
+		},
+		"PNG": {
+			DMD_LONGNAME: "Portable Network Graphics",
+			DMD_MIMETYPE: "image/png",
+			DMD_EXTENSION: "png",
+			DCAP_CREATE: undefined
+		},
+		"JPEG": {
+			DMD_LONGNAME: "JPEG JFIF",
+			DMD_MIMETYPE: "image/jpeg",
+			DMD_EXTENSION: "jpg",
+			DCAP_CREATE: undefined
 		}
 	};
 
@@ -34,6 +47,7 @@ describe('Driver', function() {
 				assert.equal(expected_meta.DMD_LONGNAME,metadata.DMD_LONGNAME);
 				assert.equal(expected_meta.DMD_MIMETYPE,metadata.DMD_MIMETYPE);
 				assert.equal(expected_meta.DMD_EXTENSION,metadata.DMD_EXTENSION);
+				assert.equal(expected_meta.DCAP_CREATE,metadata.DCAP_CREATE);
 			});
 		});
 	});
