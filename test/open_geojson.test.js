9971752b38a4bb6b72440289bfd50db877f95522
Brian Reavis <brian@thirdroute.com>
2014-06-06 15:52:18 -0600

    Test for field definitions / values.

--- test/open_geojson.test.js
+++ test/open_geojson.test.js
@@ -23,9 +23,35 @@ describe('Open', function() {
 				assert.ok(layer);
 				assert.instanceOf(layer, ogr.Layer);
 			});
-			it('should have definition', function() {
-				assert.ok(layer.getLayerDefn());
-			})
+			describe('definition', function() {
+				var defn;
+				before(function() {
+					defn = layer.getLayerDefn();
+				});
+				it('should exist', function() {
+					assert.ok(defn);
+					assert.instanceOf(defn, ogr.FeatureDefn);
+				});
+				it('should have all fields defined', function() {
+					assert.equal(defn.getFieldCount(), 3);
+					assert.equal(defn.getFieldDefn(0).getName(), 'kind');
+					assert.equal(defn.getFieldDefn(1).getName(), 'name');
+					assert.equal(defn.getFieldDefn(2).getName(), 'state');
+				});
+			});
+			describe('features', function() {
+				it('should have all fields', function() {
+					assert.equal(layer.getFeatureCount(), 1);
+					var feature = layer.getFeature(0);
+					var fields = feature.getFields();
+
+					assert.deepEqual(fields, {
+						'kind': 'county',
+						'state': 'WY',
+						'name': 'Park'
+					});
+				});
+			});
 		});
 	});
 });
