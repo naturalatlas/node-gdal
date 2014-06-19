417c566290a625732617c7306b7fe2c8e081bc43
Dane Springmeyer <dane@mapbox.com>
2014-06-19 09:23:54 -0700

    update raster info example to newer API

--- examples/rasterinfo.js
+++ examples/rasterinfo.js
@@ -1,18 +1,15 @@
-var gdal = require('../lib/index.js');
+var gdal = require('../');
 
 var filename = __dirname + "/sample.tif";
 var dataset  = gdal.open(filename);
 
-console.log("number of bands: " + dataset.getRasterCount());
-console.log("width: " + dataset.getRasterXSize());
-console.log("height: " + dataset.getRasterYSize());
-console.log("geotransform: ");
-console.log(dataset.getGeoTransform());
-console.log("");
+console.log("number of bands:",dataset.bands.count());
+//console.log(dataset)
+console.log("width:",dataset.rasterSize.x);
+console.log("height:",dataset.rasterSize.y);
+console.log("geotransform:",JSON.stringify(dataset.geoTransform));
 
-var n_bands = dataset.getRasterCount();
-for (var i = 0, n = n_bands; i < n; i++) {
-	console.log("band[" + i + "] statistics: ");
-	var band = dataset.getRasterBand(1 + i);
-	console.log(band.getStatistics(0,1))
+for (var i = 0, n = dataset.bands.count(); i < n; i++) {
+	var band = dataset.bands.get(1 + i);
+	console.log("band[" + i + "] statistics: ",JSON.stringify(band.getStatistics(true,true)))
 }
