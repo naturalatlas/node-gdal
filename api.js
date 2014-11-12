f27d169aa3b169c88fbb5f1e6dde24ef808869de
Brian Reavis <brian@thirdroute.com>
2014-11-12 14:51:58 -0700

    Updates

---
+++ api.js
@@ -0,0 +1,21 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "gdal.Envelope",
+        "gdal.Envelope3D",
+        "gdal.Feature",
+        "gdal.FeatureDefn",
+        "gdal.Layer"
+    ],
+    "modules": [
+        "gdal"
+    ],
+    "allModules": [
+        {
+            "displayName": "gdal",
+            "name": "gdal",
+            "description": "Provides a binding to libgdal."
+        }
+    ]
+} };
+});
