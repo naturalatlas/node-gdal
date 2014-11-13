c2114d2da9ac168b261af28cf125e5fbf30a3373
Brian Reavis <brian@thirdroute.com>
2014-11-13 09:37:10 -0700

    Added support for flag: [skip ci]

--- scripts/publish-docs.js
+++ scripts/publish-docs.js
@@ -1,7 +1,7 @@
 var ghpages = require('gh-pages');
 var path = require('path');
 
-ghpages.publish(path.resolve(__dirname, '../yuidocs'), function(err) {
+ghpages.publish(path.resolve(__dirname, '../yuidocs'), {message: 'Updated documentation [skip ci]'}, function(err) {
 	if (err) {
 		console.error(err);
 		process.exit(1);
