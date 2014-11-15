4d36db38721d86778be82cd5a3d786307c4ee7ec
Brian Reavis <brian@thirdroute.com>
2014-11-15 11:52:19 -0700

    Fixed gh-pages version [skip ci]

--- scripts/publish-docs.js
+++ scripts/publish-docs.js
@@ -1,11 +1,14 @@
 var ghpages = require('gh-pages');
 var path = require('path');
 
+process.stdout.write('Publishing to "gh-pages" branch... ');
 ghpages.publish(path.resolve(__dirname, '../yuidocs'), {message: 'Updated documentation [skip ci]'}, function(err) {
 	if (err) {
+		process.stdout.write('error\n');
 		console.error(err);
 		process.exit(1);
 	} else {
+		process.stdout.write('success\n');
 		process.exit(0);
 	}
 });
