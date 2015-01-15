b4b6dd02235e95ea9716d4406c57316eae3950e8
gretacb <carol@mapbox.com>
2015-01-15 15:12:34 -0500

    add SHARED tag

--- test/open_bigtiff.test.js
+++ test/open_bigtiff.test.js
@@ -4,20 +4,22 @@ var gdal = require('../lib/gdal.js');
 var path = require('path');
 var assert = require('chai').assert;
 
-describe('Open', function() {
-	
-	describe('BIGTIFF', function() {
-		var filename, ds;
+if (process.env.TARGET !== 'SHARED') {
+	describe('Open', function() {
 
-		it('should not throw', function() {
-			filename = path.join(__dirname,"data/sample_bigtiff.tif");
-			ds = gdal.open(filename);
-		});
+		describe('BIGTIFF', function() {
+			var filename, ds;
 
-		it('should be able to read raster size', function() {
-			assert.equal(ds.rasterSize.x,100000);
-			assert.equal(ds.rasterSize.y,100000);
-			assert.equal(ds.bands.count(),1);
+			it('should not throw', function() {
+				filename = path.join(__dirname,"data/sample_bigtiff.tif");
+				ds = gdal.open(filename);
+			});
+
+			it('should be able to read raster size', function() {
+				assert.equal(ds.rasterSize.x,100000);
+				assert.equal(ds.rasterSize.y,100000);
+				assert.equal(ds.bands.count(),1);
+			});
 		});
 	});
-});
+};
