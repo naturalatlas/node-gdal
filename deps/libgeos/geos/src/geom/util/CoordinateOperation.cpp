4d3b5ca2b0e64e7202b99038f4440fbe007d600f
bergwerkgis <wb@bergwerk-gis.at>
2015-10-16 12:33:17 +0000

    Windows: workaround Visual Studio 2015 bug: error C2614: 'std::bad_typeid', https://connect.microsoft.com/VisualStudio/feedback/details/1600701/type-info-does-not-compile-with-has-exceptions-0

--- deps/libgeos/geos/src/geom/util/CoordinateOperation.cpp
+++ deps/libgeos/geos/src/geom/util/CoordinateOperation.cpp
@@ -7,12 +7,14 @@
  *
  * This is free software; you can redistribute and/or modify it under
  * the terms of the GNU Lesser General Public Licence as published
- * by the Free Software Foundation. 
+ * by the Free Software Foundation.
  * See the COPYING file for more information.
  *
  **********************************************************************/
 
+#ifndef _WIN32
 #include <typeinfo>
+#endif
 
 #include <geos/geom/util/CoordinateOperation.h>
 #include <geos/geom/CoordinateSequence.h>
