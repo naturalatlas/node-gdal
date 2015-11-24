05015d4186057bdf65efc697d54721aae301a9b3
Brian Reavis <brian@thirdroute.com>
2015-11-24 13:30:00 -0700

    Replaced bigtiff test fixture (fixes #139)

--- test/open_bigtiff.test.js
+++ test/open_bigtiff.test.js
@@ -10,15 +10,19 @@ if (process.env.TARGET !== 'SHARED') {
 		describe('BIGTIFF', function() {
 			var filename, ds;
 
+			// https://github.com/naturalatlas/node-gdal/issues/139
+			// > tifftopnm sample.tif | pamscale -width=100000 -height=1 | pnmtotiff > sample_wide.tif
+			// > tiffcp -c zip -8 sample_wide.tif sample_bigtiff.tif
+
 			it('should not throw', function() {
 				filename = path.join(__dirname,"data/sample_bigtiff.tif");
 				ds = gdal.open(filename);
 			});
 
 			it('should be able to read raster size', function() {
-				assert.equal(ds.rasterSize.x,100000);
-				assert.equal(ds.rasterSize.y,100000);
-				assert.equal(ds.bands.count(),1);
+				assert.equal(ds.rasterSize.x, 100000);
+				assert.equal(ds.rasterSize.y, 1);
+				assert.equal(ds.bands.count(), 1);
 			});
 		});
 	});
