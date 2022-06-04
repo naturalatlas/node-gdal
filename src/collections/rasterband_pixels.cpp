83ccad5fe7950d0427af551a41bcd3bf298a3c5b
Jared Forsyth <jabapyth@gmail.com>
2022-06-04 14:14:06 -0600

    Update rasterband_pixels.cpp
    make it clear what the `4` constant is about

--- src/collections/rasterband_pixels.cpp
+++ src/collections/rasterband_pixels.cpp
@@ -43,8 +43,8 @@ RasterBandPixels::~RasterBandPixels()
  * 
  * Note: Typed arrays should be created with an external ArrayBuffer for versions of node >= 0.11
  * ```
- * var n = 16*16;
- * var data = new Float32Array(new ArrayBuffer(n));
+ * var n = 16 * 16;
+ * var data = new Float32Array(new ArrayBuffer(n * 4)); // 4 bytes per float
  * //read data into the existing array
  * band.pixels.read(0,0,16,16,data);```
  *
